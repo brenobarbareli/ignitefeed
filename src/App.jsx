32a0ed6b6512443da5e7958695372d1ba2dac438
brenobarbareli <breno_barbareli@hotmail.com>
2022-08-17 22:25:50 -0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import { Header } from './Components/Header';
+import { Sidebar } from './Components/Sidebar';
+
+import styles from './app.module.css';
 
 import './global.css';
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <span>testando o teste</span>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
