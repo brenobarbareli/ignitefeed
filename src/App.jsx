3799f29bd3673c04073b25c0376bbdba59894d51
brenobarbareli <breno_barbareli@hotmail.com>
2022-08-23 22:43:51 -0300

    Create the post component and structure of comment component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from './Components/Header';
 import { Sidebar } from './Components/Sidebar';
+import { Post } from './Components/post';
 
 import styles from './app.module.css';
 
@@ -12,7 +13,11 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <span>testando o teste</span>
+          <Post
+            author='Breno Barbareli'
+            content='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!'
+          />
+          <Post author='Gabriel Buzzi' content='Um novo post muito legal' />
         </main>
       </div>
     </div>
---
+++ src/Components/Comment.jsx
@@ -0,0 +1,32 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src='https://github.com/brenobarbareli.png' />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Breno Barbareli</strong>
+              <time title='11 de Maio as 08:13' dateTime='2022-05-11 08:00:00'>
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title='Deletar comentário'>
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Muito bom Breno!!</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir<span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
